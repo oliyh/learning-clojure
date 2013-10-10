d9a2d29dd4edbba54cf1cf3acc739866e8224f93
oliyh <git@oliy.co.uk>
2013-10-10 14:47:48 -0700

    basic clojure project as per lein new

---
+++ project.clj
@@ -0,0 +1,8 @@
+(defproject learning-clojure "0.1.0-SNAPSHOT"
+  :description "Walking you through the basic steps to getting to know Clojure"
+  :url "https://github.com/oliyh/learning-clojure"
+  :license {:name "Eclipse Public License"
+            :url "http://www.eclipse.org/legal/epl-v10.html"}
+  :dependencies [[org.clojure/clojure "1.5.1"]]
+  :local-repo "lib")
+
---
+++ src/learning_clojure/core.clj
@@ -0,0 +1,6 @@
+(ns learning-clojure.core)
+
+(defn foo
+  "I don't do a whole lot."
+  [x]
+  (println x "Hello, World!"))
