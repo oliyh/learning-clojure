24b96ce018f641e58801d8719e3d93d169924729
oliyh <github@oliy.co.uk>
2013-11-07 16:50:28 +0000

    Adding java example for comparison for tree challenge

--- src/learning_clojure/challenges/session2.clj
+++ src/learning_clojure/challenges/session2.clj
@@ -7,6 +7,31 @@
     (doseq [x (concat (range 1 (inc max-width) 2) [1])]
       (line x))))
 
+;; java comparison
+;;    public void printLine(int maxWidth, int numStars) {
+;;        repeat((maxWidth - numStars) / 2, " ");
+;;        repeat(numStars, "*");
+;;        System.out.print("\n");
+;;    }
+;;
+;;    private void repeat(int numStars, String s) {
+;;        for (int i = 0; i < numStars; i++) {
+;;            System.out.print(s);
+;;        }
+;;    }
+;;
+;;    @Test
+;;    public void shouldTree() {
+;;        int n = 5;
+;;
+;;        int maxWidth = (2 * n) - 3;
+;;        for (int i = 1; i <= maxWidth; i+=2) {
+;;            printLine(maxWidth, i);
+;;        }
+;;        printLine(maxWidth, 1);
+;;    }
+
+
 ;; Euler examples
 (defn multiples-of [limit & numbers]
   (reduce +
