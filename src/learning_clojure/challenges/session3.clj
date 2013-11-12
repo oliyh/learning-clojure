87ab1b6cd88945f436721e0d32385d15a2191459
Oliy <github@oliy.co.uk>
2013-11-12 22:39:48 +0000

    Adding solutions to some Euler problems

---
+++ src/learning_clojure/challenges/session3.clj
@@ -0,0 +1,44 @@
+(ns learning-clojure.challenges.session3)
+
+(defn sqrt [x] (java.lang.Math/sqrt x))
+
+;; Euler examples
+(defn multiples-of [limit & numbers]
+  (reduce +
+    (into #{}
+      (mapcat (fn [x] (range x limit x)) numbers))))
+
+(defn fib
+  ([] (fib [1 2]))
+  ([s] (let [next (apply + (take-last 2 s))
+             nnext (+ (last s) next)]
+         (concat s (lazy-seq (fib [next nnext]))))))
+
+(defn even-fib-below [limit]
+  (filter even? (take-while #(< % limit) (fib))))
+
+(defn whole? [x]
+  (zero? (rem x 1)))
+
+(defn factor?
+  ([x] #(factor? x %))
+  ([x y]
+    (zero? (rem x y))))
+
+(defn prime? [x]
+  (and (whole? x)
+    (empty? (filter #(and (whole? %) (factor? x %)) (range 2 (sqrt x))))))
+
+(defn largest-prime-factor [n]
+  (first (filter #(and (factor? n %) (prime? %)) (range (long (sqrt n)) 2 -1))))
+
+(defn palindrome? [n]
+  (let [s (str n)
+        half (/ (count s) 2)]
+    (and (whole? half) (= (take half s) (reverse (drop half s))))))
+
+(defn largest-palindrome-product []
+  (max (filter palindrome?
+         (for [x (range 100 999)
+               y (range 100 999)]
+           (* x y)))))
